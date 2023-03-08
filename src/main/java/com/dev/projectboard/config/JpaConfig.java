0e8ef1f2dc353f17343ebfd17e7f9abae9032b46
deliciousdev <deliciousdev@naver.com>
2023-03-08 09:03:40 +0900

    중간저장

---
+++ src/main/java/com/dev/projectboard/config/JpaConfig.java
@@ -0,0 +1,21 @@
+package com.dev.projectboard.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Optional;
+
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+
+    @Bean
+    public AuditorAware<String> auditorAware(){
+        // todo 임시로 해놨는데 나중에 바꿔줘야함 : 스프링 스큐리티로 인증 기능을 붙이게 될 때, 수정하자
+        return () -> Optional.of("tempname");
+    }
+}
--- src/main/java/com/dev/projectboard/domain/Article.java
+++ src/main/java/com/dev/projectboard/domain/Article.java
@@ -1,17 +1,61 @@
 package com.dev.projectboard.domain;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
+
+@Getter
+@ToString
+//@EqualsAndHashCode
+
+@Table(indexes = { //검색을 할 수 있는 항목들
+        @Index(columnList = "title"),
+        @Index(columnList = "hashtag"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy"),
+})
+@Entity
 public class Article {
 
-    private Long id; //bigint -> Long
-    private String title; // varchar -> String
-    private String content;
-    private String hashtag;
+    @Id //프라이머리 키
+    @GeneratedValue(strategy = GenerationType.IDENTITY)//키값 자동증가 , AUTO 가 기본값인데 IDENTITY로 바꿔줘야함 MySQL 에서 키값자동증가는 IDENTITY 옵션으로 해야함
+    private Long id;
 
-    //메타데이터
-    private LocalDateTime createdAt; // datetime -> LocalDateTime
-    private String createdBy; //
-    private LocalDateTime modifiedAt;
-    private String modifiedBy;
+
+    @Setter @Column(nullable = false) private String title;
+    @Setter @Column(nullable = false, length = 10000) private String content;
+
+    @Setter private String hashtag; //선택적 이라서 null 가능
+
+   //메타데이터
+    @CreatedDate @Column(nullable = false) private LocalDateTime createdAt;
+    @CreatedBy @Column(nullable = false, length = 100) private String createdBy; //
+    @LastModifiedDate private LocalDateTime modifiedAt;
+    @LastModifiedBy @Column(nullable = false,length = 100) private String modifiedBy;
+
+
+    protected Article() {
+    }
+
+    private Article(String title, String content, String hashtag) {
+        this.title = title;
+        this.content = content;
+        this.hashtag = hashtag;
+    }
+
+    public static Article of(String title, String content, String hashtag) {
+        return new Article(title, content, hashtag);
+    }
+
+
+
 }
