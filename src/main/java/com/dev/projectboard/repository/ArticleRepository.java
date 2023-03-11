7f6e88d8408276a1efc877007e553b05951706e5
deliciousdev <deliciousdev@naver.com>
2023-03-11 23:42:25 +0900

    #10 - JpaRepository @Repository 어노테이션 넣을 필요 업다는 주석 넣었음

--- src/main/java/com/dev/projectboard/repository/ArticleRepository.java
+++ src/main/java/com/dev/projectboard/repository/ArticleRepository.java
@@ -3,5 +3,7 @@ package com.dev.projectboard.repository;
 import com.dev.projectboard.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
+//@Repository 없어도됨 : JPA 구현체인 SimpleJpaRepository 에 이미 붙어있음
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
