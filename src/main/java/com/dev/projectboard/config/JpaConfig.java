cd1985e33fa22dcc14947ec4903d58a41602b424
deliciousdev <deliciousdev@naver.com>
2023-03-11 23:40:41 +0900

    #10 설정파일들

--- src/main/java/com/dev/projectboard/config/JpaConfig.java
+++ src/main/java/com/dev/projectboard/config/JpaConfig.java
@@ -14,8 +14,10 @@ import java.util.Optional;
 public class JpaConfig {
 
     @Bean
-    public AuditorAware<String> auditorAware(){
+    public AuditorAware<String> auditorAware(){//사람 이름이니까 타입은 String
         // todo 임시로 해놨는데 나중에 바꿔줘야함 : 스프링 스큐리티로 인증 기능을 붙이게 될 때, 수정하자
-        return () -> Optional.of("tempname");
+        //Optional 반환값을 줘야함
+        //jpa 가 auditing 할때마다 이 이름을 넣어줌
+        return () -> Optional.of("eno");
     }
 }
