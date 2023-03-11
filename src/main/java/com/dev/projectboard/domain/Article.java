f1c90e369666c4973f418301ff6a2c24246b238b
deliciousdev <deliciousdev@naver.com>
2023-03-11 23:17:41 +0900

    #10 - 도메인 코드를 엔티티로 변환 도메인 클래스와 테이블이 1:1 매칭되도록 설계 했다. 생성자와 equlas() hashcode() 등이 도메인 맥락에 맞게끔 디테일하게 설정함

--- src/main/java/com/dev/projectboard/domain/Article.java
+++ src/main/java/com/dev/projectboard/domain/Article.java
@@ -8,10 +8,13 @@ import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Set;
 
 
 @Getter
@@ -24,6 +27,7 @@ import java.util.Objects;
         @Index(columnList = "createdAt"),
         @Index(columnList = "createdBy"),
 })
+@EntityListeners(AuditingEntityListener.class)//엔티티에는 Auditing이 쓰인다는 표시를 해줘야함
 @Entity
 public class Article {
 
@@ -43,18 +47,27 @@ public class Article {
     @Setter
     private String hashtag; //선택적 이라서 null 가능
 
+
+    //양방향 바인딩을 완성 : List, Set Map 등 용도에맞게 원하는걸로 하면됨 :
+    //Set 의 의도 : 중복을 허용하지 않겠다.
+    //cascade 가 걸려있으면 연관 되어있기 때문에, 얘 를 삭제하면 딸린 식구들까지 다 삭제됨(FK를 걸어주는 개념임 : 부모없는 자식을 만들지 않겠다는 의미: 부모가 삭제되면 자식도 자동으로 삭제 시키겠다는 의미)
+    //cascade 걸어줄지 말지는 팀에서 상의해서 정하는거임
+    @OrderBy("id")//정렬기준은 id로 할거임
+    @OneToMany(mappedBy = "article", cascade= CascadeType.ALL)
+    @ToString.Exclude //article 이라고 이름을 정해주지않으면 두 엔티티 이름을 합쳐서 테이블을 따로 만듬
+    private final Set<ArticleComment> articleComments = new LinkedHashSet<>();
+
+
+
     //메타데이터
-    @CreatedDate
-    @Column(nullable = false)
-    private LocalDateTime createdAt;
-    @CreatedBy
-    @Column(nullable = false, length = 100)
-    private String createdBy; //
-    @LastModifiedDate
-    private LocalDateTime modifiedAt;
-    @LastModifiedBy
-    @Column(nullable = false, length = 100)
-    private String modifiedBy;
+    @CreatedDate @Column(nullable = false) private LocalDateTime createdAt;
+
+    @CreatedBy @Column(nullable = false, length = 100) private String createdBy; //
+
+    @LastModifiedDate @Column(nullable = false) private LocalDateTime modifiedAt;
+
+    @LastModifiedBy @Column(nullable = false, length = 100) private String modifiedBy;
+
 
 
     protected Article() {
--- src/main/java/com/dev/projectboard/domain/ArticleComment.java
+++ src/main/java/com/dev/projectboard/domain/ArticleComment.java
@@ -1,12 +1,11 @@
 package com.dev.projectboard.domain;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.time.LocalDate;
@@ -14,6 +13,7 @@ import java.time.LocalDateTime;
 import java.util.Objects;
 
 
+//@NoArgsConstructor(access = AccessLevel.PROTECTED) //이런식으로 생성자를 만들 수도 있음
 @Getter
 @ToString
 
@@ -22,6 +22,8 @@ import java.util.Objects;
        @Index(columnList = "createdAt"),
        @Index(columnList = "createdBy")
 })
+
+@EntityListeners(AuditingEntityListener.class)
 @Entity
 public class ArticleComment {
 
@@ -32,13 +34,14 @@ public class ArticleComment {
     // table 에서는 Article 의 id를 fk로 갖음 : 클래스에서는 객체지향적으로 객체를 갖고있으면됨. (DB테이블상에서, fk가 아닌 단순 article 참조여도됨)
     // optional= false : Article은 필수로 있어야하는값임
     //cascade ={} :디폴트 : ArticleCommment를 지웠을때 , 관련이있는 Article 이 영향을 받지않음 ( 댓글이 지워지든 말든 본문입장에선 상관없음) : 이런 경우 그냥 cascade 옵션을 안적어도됨
-    @Setter @ManyToOne(optional = false, cascade = {}) private Article article ;
+    //@ManyToOne 으로 매핑 해줬음.
+    @Setter @ManyToOne(optional = false) private Article article ;
     @Setter @Column(nullable = false , length = 500) private String content;
 
     //메타데이터
     @CreatedDate @Column(nullable = false) private LocalDateTime createdAt;
     @CreatedBy @Column(nullable = false) private String createdBy;
-    @LastModifiedDate @Column(nullable = false) private LocalDate modifiedAt;
+    @LastModifiedDate @Column(nullable = false) private LocalDateTime modifiedAt;
     @LastModifiedBy @Column(nullable = false) private String modifiedBy;
 
     protected ArticleComment() {
