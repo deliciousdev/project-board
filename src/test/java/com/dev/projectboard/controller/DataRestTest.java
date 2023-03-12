de516adad09c032045a18e9f3c30e6d35787b4e3
deliciousdev <deliciousdev@naver.com>
2023-03-12 21:17:11 +0900

    #1 - Spring Data REST 테스트 제외 처리 공부 목적으로도 의미는 있기 때문에 삭제하지는 않고 제외 처리해서 전체 테스트 중에 실행되지 않게끔 처리

---
+++ src/test/java/com/dev/projectboard/controller/DataRestTest.java
@@ -0,0 +1,82 @@
+package com.dev.projectboard.controller;
+
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.transaction.annotation.Transactional;
+
+//@WebMvcTest
+@Disabled("Spring Data Rest 통합테스트는 불필요하므로 제외시킴")
+@DisplayName("Data Rest - API 테스트")
+@AutoConfigureMockMvc
+@SpringBootTest
+@Transactional
+public class DataRestTest {
+    private final MockMvc mvc;
+
+    //@Autowired 를 안넣어주면 에러뜨네
+    public DataRestTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @DisplayName("[api] 게시글 리스트 조회")
+    @Test
+    void test() throws Exception {
+
+        mvc.perform(MockMvcRequestBuilders.get("/api/articles"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.valueOf("application/hal+json")));
+    }
+
+    @DisplayName("[api] 게시글 단건 조회")
+    @Test
+    void test2() throws Exception {
+
+        mvc.perform(MockMvcRequestBuilders.get("/api/articles/1"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.valueOf("application/hal+json")))
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+    @DisplayName("[api] article-> 댓글 리스트 조회")
+    @Test
+    void test3() throws Exception {
+
+        mvc.perform(MockMvcRequestBuilders.get("/api/articles/1/articleComments"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.valueOf("application/hal+json")));
+    }
+
+    @DisplayName("[api] 직접 댓글 리스트 조회")
+    @Test
+    void test4() throws Exception {
+
+        mvc.perform(MockMvcRequestBuilders.get("/api/articleComments"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.valueOf("application/hal+json")));
+    }
+
+    @DisplayName("[api] 댓글 단건 조회")
+    @Test
+    void test5() throws Exception {
+
+        mvc.perform(MockMvcRequestBuilders.get("/api/articleComments"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.valueOf("application/hal+json")))
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+
+
+
+}
